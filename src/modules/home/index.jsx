53113e203e142ed69eae58bbe7830aadbca2a7ff
thinkorange2026 <thinkorange2026@gmail.com>
2026-09-05 19:47:19 +0530

    sfs

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -66,7 +66,7 @@ export default function Home() {
       <WhenToCallUs />
       <DscBand />
       <PartnerProgramme />
-      <Testimonial />
+      {/* <Testimonial /> */}
       <Faqs />
       <Insights />
       {/* <TrustStrip /> */}
